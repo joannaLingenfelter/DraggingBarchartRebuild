9196c973cb890f77551793f78ec95279451454a7
Jo Lingenfelter <joanna.lingenfelter@willowtreeapps.com>
2023-04-21 14:01:36 -0400

    Initial Commit

---
+++ DraggingBarChartRebuild/DraggingBarChartRebuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraggingBarChartRebuildApp.swift
+//  DraggingBarChartRebuild
+//
+//  Created by Jo Lingenfelter on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct DraggingBarChartRebuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
