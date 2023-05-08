524d6caf6f3dce470707f38ae3582d92e2fba29d
Jo Lingenfelter <joanna.lingenfelter@willowtreeapps.com>
2023-05-07 21:26:05 -0400

    Fix crash indexing data

--- DraggingBarChartRebuild/DataSource.swift
+++ DraggingBarChartRebuild/DataSource.swift
@@ -19,7 +19,7 @@ class DataSource: ObservableObject {
     private let allData: [ChartData]
 
     private(set) var upperBound: CGFloat = 0
-    private(set) var data: [ChartData] = []
+    private(set) var slicedData: [ChartData] = []
 //    private(set) var yAxisMarkValues: [Int] = []
 
     private static let calendar = Calendar(identifier: .gregorian)
@@ -41,6 +41,7 @@ class DataSource: ObservableObject {
     func setLeadingVisibleData(_ initialData: ChartData) {
         print("*** setLeadingVisibleData:")
         print("***    - initial: \(initialData.date.formatted(.dateTime.day().month().year()))")
+
         guard let index = self.allData.firstIndex(of: initialData) else {
             return
         }
@@ -48,19 +49,8 @@ class DataSource: ObservableObject {
         let desiredRange = (index - visibleBarCount) ..< (index + 2 * visibleBarCount)
 
         print("***    - desiredRange: \(desiredRange)")
-//        var clampedRange = desiredRange.clamped(to: allData.indices)
-//        if clampedRange.count < 3 * visibleBarCount {
-//            clampedRange = clampedRange.lowerBound ..< clampedRange.lowerBound + (3 * visibleBarCount)
-////            clampedRange.upperBound += (3 * visibleBarCount) - clampedRange.count
-//        }
-//        print("***    - clampedRange: \(clampedRange)")
 
-//        var newData = [ChartData]()
-//        let leadingDifference = max(clampedRange.lowerBound - desiredRange.lowerBound, 0)
-//        for index in (0..<leadingDifference) {
-//            newData.insert(ChartData(date: <#T##Date#>, value: <#T##Double#>), at: <#T##Int#>)
-//        }
-        data = desiredRange.map { index in
+        slicedData = desiredRange.map { index in
             if allData.indices.contains(index) {
                 print("*** Got a match: \(index)")
                 return allData[index]
@@ -71,37 +61,30 @@ class DataSource: ObservableObject {
                 return ChartData(date: date, value: 0.0, isVirtual: true)
             }
         }
-        print("***    - data.count: \(data.count)")
+        print("***    - data.count: \(slicedData.count)")
 
-        let visibleData = data.filter { !$0.isVirtual }.prefix(visibleBarCount)
+        let filteredSlicedData = slicedData.filter { !$0.isVirtual }
+        print("*** unpadded.count: \(filteredSlicedData.count)")
+
+        let paddingCount = slicedData.count - filteredSlicedData.count
+        print("*** paddingCount: \(paddingCount)")
+
+        let paddedIndex = index + paddingCount
+        print("*** paddedIndex: \(index)")
+
+        let offSet = Int(index % filteredSlicedData.count)
+        print("*** offSet: \(offSet)")
+        let clampedOffset = min(offSet, ((visibleBarCount * 2) - 1))
+        let clampedUpperBound = min(clampedOffset + visibleBarCount, filteredSlicedData.count - 1)
+        let adjustedIndex = index < filteredSlicedData.count ? index : index - offSet
+        print("*** adjustedIndex: \(adjustedIndex)")
+
+        let visibleData = slicedData.filter { !$0.isVirtual }[clampedOffset..<clampedUpperBound]
+        print("*** - visibleData: \(visibleData.map { $0.date.formatted(.dateTime.month().day())})")
         upperBound = visibleData.map(\.value).max() ?? .zero
         print("***    - upperBound: \(upperBound)")
-        
-//        data = (0..<(3*visibleBarCount)).map { i -> ChartData in
-//            let totalOffset = i + offset
-//            let startOfToday = calendar.startOfDay(for: .now)
-//            let date = calendar.date(byAdding: .day, value: totalOffset, to: startOfToday)!
-//            let value = CGFloat(totalOffset).magnitude * CGFloat(10)
-//            return .init(date: date, value: value)
-//        }
-//        upperBound = data[0..<visibleBarCount].map(\.value).max() ?? 0
-//        yAxisMarkValues = (stride(from: 0, through: upperBound, by: upperBound / 3.0).map { $0 } + [upperBound * 1.25]).map(Int.init)
         objectWillChange.send()
     }
-
-//    private func data(for offset: Int) -> [ChartData] {
-//        (0..<(3*visibleBarCount)).map { i -> ChartData in
-//            let totalOffset = i + offset
-//            let startOfToday = calendar.startOfDay(for: .now)
-//            let date = calendar.date(byAdding: .day, value: totalOffset, to: startOfToday)!
-//            let value = CGFloat(totalOffset).magnitude * CGFloat(10)
-//            return .init(date: date, value: value)
-//        }
-//    }
-
-//    private func indexOfChartData(_ data: ChartData) -> Int? {
-//        self.data.firstIndex(of: data)
-//    }
 
     func chartData(closestTo date: Date) -> ChartData? {
         allData.sorted { lhs, rhs in
@@ -118,7 +101,7 @@ class DataSource: ObservableObject {
 
     func visibleData(of data: ChartData?) -> ChartData? {
         guard let data else { return nil }
-        let isPresent = self.data.contains { value in
+        let isPresent = self.slicedData.contains { value in
             value.date == data.date && value.value == data.value
         }
         guard isPresent else {
